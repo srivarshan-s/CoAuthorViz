31c249bdbe963b6919fb094c367d85b395625f97
Srivarshan <srivarshan02@gmail.com>
2022-12-30 12:20:38 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+
+def read_file(dir, index):
+    paths = [
+        os.path.join(dir, path) 
+        for path in os.listdir(dir) 
+        if path.endswith('jsonl')
+    ]
+    author_file = read_writing_session(paths[index])
+    return author_file
+
+
+def read_writing_session(path):
+    events = []
+    with open(path, 'r') as f:
+        for event in f:
+            events.append(json.loads(event))
+    print(f'Successfully read {len(events)} events in a writing session from {path}')
+    return events
+
