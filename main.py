0116d1bb449403ac995b05b255020501689c5b6f
Srivarshan Selvaraj <srivarshan02@gmail.com>
2023-01-02 19:40:50 +0530

    Major code logic overhaul

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import time
 
 from utils import load_sessions, read_session, read_file
-from events import *
+from operations import build_text
 
 
 def main():
@@ -13,54 +13,16 @@ def main():
     # events = read_session(sessions[0])
 
     text_buffer = []
-    cursor_pos = 0
-    cursor_select_flag = False
-    cursor_range = None
 
     for event in events:
-
-        if len(text_buffer) == 0:
-            buffer = ""
-        else:
-            buffer = text_buffer[-1]
-
-        if event["eventName"] == "system-initialize":
-            buffer, cursor_pos = system_initialize(event)
-        if event["eventName"] in ["cursor-backward", "cursor-forward"]:
-            cursor_pos, cursor_select_flag, cursor_range = move_cursor(event)
-        if event["eventName"] == "cursor-select":
-            cursor_select_flag, cursor_range = cursor_select(event)
-        if event["eventName"] == "suggestion-close":
-            suggestion_close()
-        if event["eventName"] == "suggestion-down":
-            suggestion_down()
-        if event["eventName"] == "suggestion-get":
-            suggestion_get()
-        if event["eventName"] == "suggestion-hover":
-            suggestion_hover()
-        if event["eventName"] == "suggestion-open":
-            suggestion_open()
-        if event["eventName"] == "suggestion-reopen":
-            suggestion_reopen()
-        if event["eventName"] == "suggestion-select":
-            suggestion_select()
-        if event["eventName"] == "suggestion-up":
-            suggestion_up()
-        if event["eventName"] == "text-insert":
-            buffer, cursor_pos, cursor_select_flag, cursor_range = text_insert(
-                buffer, event, cursor_pos, cursor_select_flag, cursor_range)
-        if event["eventName"] == "text-delete":
-            buffer, cursor_pos, cursor_select_flag, cursor_range = text_delete(
-                buffer, event, cursor_pos, cursor_select_flag, cursor_range)
-
+        buffer = build_text(text_buffer, event)
         text_buffer.append(buffer)
+        # os.system('clear')
+        # print(buffer)
+        # time.sleep(0.001)
 
-        os.system('clear')
-        print(buffer)
-        time.sleep(0.001)
-        # time.sleep(1)
-
-    # print(text_buffer[-1])
+    print(text_buffer[-1])
+    print(len(text_buffer), len(events))
 
 
 if __name__ == "__main__":
