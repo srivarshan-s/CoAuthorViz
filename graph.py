53ea04770b0c800ea4d844665f694c99f8b2110c
Srivarshan Selvaraj <srivarshan02@gmail.com>
2023-01-03 13:06:36 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,88 +1,81 @@
-# draw_circle.py
 from PIL import Image, ImageDraw, ImageFont
 import math
 
+
 image = Image.new("RGB", (600, 600), "white")
 draw = ImageDraw.Draw(image)
-x, y = 25, 50
+#x, y = 25, 50
 margin = 75
-line_center = 85
-x_line, y_line = 175, 100
+y_center = 45
+x1_line, x2_line = 100, 70
 font = ImageFont.load_default()
 
-
-def user_circle(num):
-    increment = num * 150
-    draw.ellipse((x+increment, y, x+margin, y+margin),
-                 fill=(100, 100, 255, 255), outline="black", width=2)
-    draw.text((x+20,y+80), "(2,1)", fill='black', font=font)
+circle_size = 50
 
 
-def gpt_circle(num):
-    increment = num * 150
-    draw.ellipse((x+increment, y, x+increment+margin, y+margin),
-                 fill=(100, 255, 100, 255), outline="black", width=2)
+def text(text, y):
+    y -= 1
+    y_start = 20 + (circle_size*2/5) + 80 * y
+    x_start = 20
+    draw.text((x_start,y_start), text, fill='black', font=font)
 
 
-def modified_circle(num):
-    increment = num * 150
-    draw.ellipse((x+increment, y, x+increment+margin, y+margin),
-                 fill=(255, 100, 100, 255), outline="black", width=2)
+def user_circle(x, y):
+    x -= 1
+    y -= 1
+    x_start = 20 + 80 * x
+    y_start = 20 + 80 * y
+    draw.ellipse((x_start, y_start, x_start+circle_size, y_start+circle_size), 
+                 fill = "pink", outline ='black')
+    #draw.text((x+20,y+80), "(2,1)", fill='black', font=font)
 
 
-def suggestion_open(num):
-    increment = num * 150
-    draw.line([(x_line+increment, line_center),
-              (y_line+increment, line_center)], fill='red', width=3)
+def gpt_circle(x, y):
+    x -= 1
+    y -= 1
+    x_start = 20 + 80 * x
+    y_start = 20 + 80 * y
+    draw.ellipse((x_start, y_start, x_start+circle_size, y_start+circle_size), 
+                 fill = (100, 255, 100, 255), outline ='black')
 
 
-def suggestion_close(num):
-    increment = num * 150
-    draw.line([(x_line+increment, line_center),
-              (y_line+increment, line_center)], fill='black', width=3)
+def modified_circle(x, y):
+    x -= 1
+    y -= 1
+    x_start = 20 + 80 * x
+    y_start = 20 + 80 * y
+    draw.ellipse((x_start, y_start, x_start+circle_size, y_start+circle_size), 
+                 fill = (255, 100, 100, 255), outline ='black')
+    
 
-# draw.line([(x_line+300, line_center), (y_line+300, line_center)], fill='red', width=3)
+def suggestion_open(x, y):
+    x_increment = x * 80
+    y_increment = y * 80
+    draw.line([(x1_line+x_increment, y_center + y_increment),
+              (x2_line+x_increment, y_center + y_increment)], fill='red', width=3)
 
-# 50, 150
+
+def suggestion_close(x, y):
+   x_increment = x * 80
+   y_increment = y * 80
+   draw.line([(x1_line+x_increment, y_center + y_increment),
+              (x2_line+x_increment, y_center + y_increment)], fill='black', width=3)
 
 
-# if _name_ == "_main_":
 if __name__ == "__main__":
-    count_circle = 0
-    count_line = 0
-    for i in range(6):
-        if count_circle == 0 or count_line == 0:
-            if i == 1:
-                user_circle(count_circle)
-                count_circle += 1
-            if i == 2:
-                gpt_circle(count_circle)
-                count_circle += 1
-            if i == 3:
-                modified_circle(count_circle)
-                count_circle += 1
-            if i == 4:
-                suggestion_open(count_line)
-                count_line += 1
-            if i == 5:
-                suggestion_close(count_line)
-                count_line += 1
-        else:
-            if i == 1:
-                user_circle(count_circle)
-                count_circle += 1
-            if i==2:
-                gpt_circle(count_circle)
-                count_circle += 1
-            if i==3:
-                modified_circle(count_circle)
-                count_circle += 1
-            if i==4:
-                suggestion_open(count_line)
-                count_line += 1
-            if i==5:
-                suggestion_close(count_line)
-                count_line += 1 
-        
+
+#start x axis for circles from 2, since text needs to be inserted
+    user_circle(2, 4)
+    gpt_circle(2, 1)
+    gpt_circle(2, 2)
+    gpt_circle(2, 3)
+    gpt_circle(3, 2)
+    gpt_circle(3, 3)
+    gpt_circle(4, 3)
+    modified_circle(2, 1)
+    modified_circle(3, 1)
+    suggestion_open(1,1)
+    suggestion_close(2, 2)
+    text("Sentence 1", 1)
+
     image.save("graph.jpg")
-    #circle("circle.jpg")
